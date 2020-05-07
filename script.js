e903d455127f73b1a0fedd84a5e5f772f92158b0
danyriansyah <danyriansyahp@gmail.com>
2020-05-07 19:36:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+            function clock() {
+              var now = new Date();
+              var secs = ('0' + now.getSeconds()).slice(-2);
+              var mins = ('0' + now.getMinutes()).slice(-2);
+              var hr = now.getHours();
+              var Time = hr + ":" + mins + ":" + secs;
+              document.getElementById("watch").innerHTML = Time;
+              requestAnimationFrame(clock);
+            }
+            requestAnimationFrame(clock);
+        });
+        
